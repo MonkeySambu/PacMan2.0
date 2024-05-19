74be59560e0f05014cf29fa0cee5b0557ac3138f
ZELA2000 <79649375+ZELA2000@users.noreply.github.com>
2024-05-19 17:59:24 +0200

    Comprensione di JAVAFX

--- src/main/java/prova/pacman/HelloApplication.java
+++ src/main/java/prova/pacman/HelloApplication.java
@@ -11,7 +11,8 @@ public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        Scene scene = new Scene(fxmlLoader.load(), 1000, 900);
+        stage.setFullScreen(true);
         stage.setTitle("Hello!");
         stage.setScene(scene);
         stage.show();
