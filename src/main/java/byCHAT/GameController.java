256a2bf81f620b454030684187a393fc6a567136
MonkeySambu <113924005+MonkeySambu@users.noreply.github.com>
2024-05-30 16:59:04 +0200

    Player circa funzionate e campo al 90%

--- src/main/java/byCHAT/GameController.java
+++ src/main/java/byCHAT/GameController.java
@@ -14,26 +14,6 @@ public class GameController {
 
     @FXML
     public void initialize() {
-        //pacMan = new PacMan();
-       //gameBoard.add(pacMan, 10, 10);
-        //gameBoard.add(ghost, 15, 15);
 
-        /*gameBoard.setOnKeyPressed(event -> {
-            switch (event.getCode()) {
-                case UP:
-                    GridPane.setRowIndex(pacMan, GridPane.getRowIndex(pacMan) - 1);
-                    break;
-                case DOWN:
-                    GridPane.setRowIndex(pacMan, GridPane.getRowIndex(pacMan) + 1);
-                    break;
-                case LEFT:
-                    GridPane.setColumnIndex(pacMan, GridPane.getColumnIndex(pacMan) - 1);
-                    break;
-                case RIGHT:
-                    GridPane.setColumnIndex(pacMan, GridPane.getColumnIndex(pacMan) + 1);
-                    break;
-            }
-        });*/
-        //gameBoard.requestFocus();
     }
 }
--- src/main/java/byCHAT/PacManGame.java
+++ src/main/java/byCHAT/PacManGame.java
@@ -1,10 +1,14 @@
 package byCHAT;
 
 import javafx.application.Application;
+import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.input.KeyEvent;
 import javafx.scene.layout.Pane;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
 import javafx.stage.Stage;
 import prova.pacman.HelloApplication;
 
@@ -13,13 +17,44 @@ public class PacManGame extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception {
         FXMLLoader loader = new FXMLLoader(PacManGame.class.getResource("campo.fxml"));
-        Scene scene = new Scene(loader.load(), 1280, 720);
+        Parent root = loader.load();
+        Scene scene = new Scene(root, 1280, 720);
         //scene.getStylesheets().add(getClass().getResource("/byCHAT/style.css").toExternalForm());
         primaryStage.setTitle("Pac-Man");
         primaryStage.setScene(scene);
         primaryStage.setResizable(false);
+
+        Pacman pacman = new Pacman(100,70,70,70,Color.YELLOW);
+
+        // Ottenere il riferimento al nodo radice e aggiungere il rettangolo
+        Pane pane = (Pane) root;
+        pane.getChildren().add(pacman);
+
+        scene.setOnKeyPressed(new EventHandler<KeyEvent>() {
+            @Override
+            public void handle(KeyEvent event) {
+                switch (event.getCode()) {
+                    case UP:
+                    case W:
+                        pacman.moveUp();
+                        break;
+                    case DOWN:
+                    case S:
+                        pacman.moveDown();
+                        break;
+                    case LEFT:
+                    case A:
+                        pacman.moveLeft();
+                        break;
+                    case RIGHT:
+                    case D:
+                        pacman.moveRight();
+                        break;
+                }
+            }
+        });
+
         primaryStage.show();
-        //FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml")
     }
 
     public static void main(String[] args) {
