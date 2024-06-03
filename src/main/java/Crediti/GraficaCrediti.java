263b55d15110c0831ad67da10b5e55e87accc7ea
MonkeySambu <113924005+MonkeySambu@users.noreply.github.com>
2024-06-03 19:52:14 +0200

    Crediti fix

--- src/main/java/Crediti/GraficaCrediti.java
+++ src/main/java/Crediti/GraficaCrediti.java
@@ -11,7 +11,7 @@ import java.io.IOException;
 public class GraficaCrediti extends Application {
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(GraficaCrediti.class.getResource("crediti.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 1280, 720);
+        Scene scene = new Scene(fxmlLoader.load(), 1024, 720);
         stage.setTitle("Pac-Man Java Edition -- Crediti");
         stage.setScene(scene);
         stage.show();
